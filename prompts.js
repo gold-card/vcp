17e8380fdfe59409e28231c42dbfefaa31ed39df
renxusheng <782449909@qq.com>
2020-11-15 17:04:17 +0800

    🚀 feat: 初版实现.

--- prompts.js
+++ prompts.js
@@ -1,26 +1,26 @@
 module.exports = [
-  // 选择ui框架 element-ui/iview - 默认为element-ui
+  // 选择ui框架
   {
-    name: "ui-framework",
-    type: "list",
-    message: "Please Choose UI Framework(default Element UI)",
+    name: 'ui-framework',
+    type: 'list',
+    message: 'Please Choose UI Framework',
     choices: [
       {
-        name: "Element UI",
-        value: "element-ui",
+        name: 'Element UI',
+        value: 'element-ui',
       },
       {
-        name: "Ant Design Vue",
-        value: "ant-design-vue",
+        name: 'Ant Design Vue',
+        value: 'ant-design-vue',
       },
     ],
-    default: "element-ui",
+    default: 'element-ui',
   },
   // 是否使用echarts
   {
-    name: "echarts",
-    type: "confirm",
+    name: 'echarts',
+    type: 'confirm',
     message: `Install Echarts ?`,
     default: true,
   },
-];
+]
